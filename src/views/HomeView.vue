68494b55c2b90bc3bc242536727d47c7771348cd
Jay <kred110396@gmail.com>
2024-07-22 01:07:46 +0800

    feat: 🎸 add room-intro section to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import AppHeader from '@/components/global/AppHeader.vue';
+
+import { Icon } from '@iconify/vue';
 </script>
 
 <template>
@@ -177,6 +179,48 @@ import AppHeader from '@/components/global/AppHeader.vue';
         </div>
       </div>
     </section>
+
+    <section class="room-intro position-relative px-3 py-20 px-md-0 py-md-30 bg-neutral-120">
+      <div class="d-flex flex-column flex-md-row justify-content-center align-items-center justify-content-md-start align-items-md-end gap-6 gap-md-20">
+        <picture class="position-relative z-1">
+          <source
+            srcset="@/assets/images/home-room-1.png"
+            media="(min-width:375px)"
+          >
+          <img
+            src="@/assets/images/home-room-sm-1.png"
+            alt="room-a"
+          >
+        </picture>
+        <div class="room-intro-content text-neutral-0">
+          <h2 class="mb-2 mb-md-4 fw-bold">
+            尊爵雙人房
+          </h2>
+          <p class="mb-6 mb-md-10">
+            享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。
+          </p>
+          <div class="mb-6 mb-md-10 fs-3 fw-bold">
+            NT$ 10,000
+          </div>
+          <RouterLink class="btn btn-neutral-0 d-flex justify-content-end align-items-center gap-3 w-100 p-5 p-md-10 mb-6 mb-md-10 text-end text-neutral-100 fs-5 fw-semibold border-0">
+            查看更多
+            <div class="cta-deco" />
+          </RouterLink>
+          <div class="d-flex justify-content-end text-primary-100">
+            <Icon
+              icon="mdi:arrow-left"
+              class="m-4"
+              style="font-size: 1.5rem;"
+            />
+            <Icon
+              icon="mdi:arrow-right"
+              class="m-4"
+              style="font-size: 1.5rem;"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -189,7 +233,8 @@ $grid-breakpoints: (
   md: 768px,
   lg: 992px,
   xl: 1200px,
-  xxl: 1400px
+  xxl: 1400px,
+  xxxl: 1537px
 );
 
 section .btn {
@@ -398,6 +443,100 @@ section .btn {
 
   @include media-breakpoint-down(md) {
     font-size: 14px;
+  }
+}
+
+
+.room-intro::before {
+  position: absolute;
+  top: -24px;
+  right: -80px;
+  z-index: 2;
+  content: '';
+  width: 375px;
+  height: 84px;
+  background-image: url('@/assets/images/deco-line-group-horizontal-sm.svg');
+  background-repeat: no-repeat;
+
+
+  @include media-breakpoint-up(md) {
+    background-image: url('@/assets/images/deco-line-group-horizontal.svg');
+    width: 1060px;
+    height: 187px;
+    top: -50px;
+    left: 430px;
+    right: auto;
+  }
+
+  @include media-breakpoint-up(lg) {
+    top: 0px;
+    left: 430px;
+  }
+
+  @include media-breakpoint-up(xl) {
+    top: 130px;
+    left: 630px;
+  }
+
+  @include media-breakpoint-up(xxxl) {
+    top: 180px;
+    left: 860px;
+  }
+}
+
+.room-intro::after {
+  position: absolute;
+  z-index: -1;
+  bottom: 265px;
+  right: 0;
+  content: '';
+  width: 375px;
+  height: 132px;
+  background-image: url('@/assets/images/deco-wave-bg-sm.svg');
+  background-repeat: no-repeat;
+
+
+  @include media-breakpoint-up(md) {
+    background-image: url('@/assets/images/deco-wave-bg.svg');
+    width: 1920px;
+    height: 86%;
+    bottom: 0;
+    left: -280px;
+    right: auto;
+  }
+
+  @include media-breakpoint-up(lg) {
+    height: 80%;
+  }
+
+  @include media-breakpoint-up(xl) {
+    height: 62.5%;
+    left: -100px;
+  }
+
+  @include media-breakpoint-up(xxxl) {
+    height: 677px;
+    bottom: 0;
+    left: 0;
+  }
+}
+
+.room-intro img{
+  --origin-width: 900;
+  --container-width: 1920;
+  --percent-width: calc(var(--origin-width) / var(--container-width) * 100vw);
+  max-width: var(--percent-width);
+
+  @include media-breakpoint-down(md) {
+    max-width: 100%;
+  }
+}
+
+.room-intro-content {
+  max-width: 628px;
+
+  @include media-breakpoint-up(xxxl) {
+    flex-grow: 1;
   }
 }
 
