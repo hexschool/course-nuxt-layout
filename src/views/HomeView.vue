9d9f41d43482ffe7faba0f14d7da351897ce0fda
Jay <kred110396@gmail.com>
2024-07-22 18:31:24 +0800

    feat: 🎸 add delicacy section to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -221,6 +221,181 @@ import { Icon } from '@iconify/vue';
         </div>
       </div>
     </section>
+
+    <section class="delicacy position-relative py-20 py-md-30 bg-primary-10">
+      <div class="container">
+        <div class="d-flex align-items-center gap-10 mb-10 mb-md-20">
+          <h2 class="mb-0 fs-1 fw-bold text-primary-100">
+            佳餚
+            <div>
+              美饌
+            </div>
+          </h2>
+          <div class="deco-line" />
+        </div>
+        <div class="row flex-nowrap overflow-x-auto">
+          <div class="col-10 col-md-6 col-xl-4">
+            <div
+              class="card position-relative border-0 rounded-3"
+            >
+              <picture>
+                <source
+                  srcset="@/assets/images/home-food-1.png"
+                  media="(min-width: 576px)"
+                >
+                <img
+                  class="w-100 rounded-3"
+                  src="@/assets/images/home-food-sm-1.png"
+                  alt="海霸"
+                >
+              </picture>
+              <div class="card-body position-absolute bottom-0 p-4 p-md-6 rounded-bottom-3  text-neutral-0">
+                <div class="d-flex justify-content-between align-items-center mb-4 mb-md-6">
+                  <h5 class="card-title mb-0 fw-bold">
+                    海霸
+                  </h5>
+                  <div class="d-flex justify-content-between gap-4 text-neutral-40">
+                    <span class="fw-bold">SUN-MON</span>
+                    <span class="fw-bold">11:00 - 20:30</span>
+                  </div>
+                </div>
+                <p class="card-text fw-medium">
+                  以新鮮海產料理聞名，我們的專業廚師選用高雄當地的海鮮，每一道菜都充滿海洋的鮮美與清甜。無論是烤魚、蒸蝦還是煮蛤蜊，都能讓您品嚐到最新鮮的海洋風味。
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-10 col-md-6 col-xl-4">
+            <div
+              class="card position-relative border-0 rounded-3"
+            >
+              <picture>
+                <source
+                  srcset="@/assets/images/home-food-2.png"
+                  media="(min-width: 576px)"
+                >
+                <img
+                  class="w-100 rounded-3"
+                  src="@/assets/images/home-food-sm-2.png"
+                  alt="日食"
+                >
+              </picture>
+              <div class="card-body position-absolute bottom-0 p-4 p-md-6 rounded-bottom-3  text-neutral-0">
+                <div class="d-flex justify-content-between align-items-center mb-4 mb-md-6">
+                  <h5 class="card-title mb-0 fw-bold">
+                    日食
+                  </h5>
+                  <div class="d-flex justify-content-between gap-4 text-neutral-40">
+                    <span class="fw-bold">SUN-MON</span>
+                    <span class="fw-bold">17:00 - 22:00</span>
+                  </div>
+                </div>
+                <p class="card-text fw-medium">
+                  為您提供優質的牛排，每一塊肉都來自頂級的牛肉，經過專業廚師的巧手烹調，口感豐滿、風味絕佳。搭配我們的特製醬料，讓您的味蕾享受一場美味的盛宴。
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-10 col-md-6 col-xl-4">
+            <div
+              class="card position-relative border-0 rounded-3"
+            >
+              <picture>
+                <source
+                  srcset="@/assets/images/home-food-3.png"
+                  media="(min-width: 576px)"
+                >
+                <img
+                  class="w-100 rounded-3"
+                  src="@/assets/images/home-food-sm-3.png"
+                  alt="山臻"
+                >
+              </picture>
+              <div class="card-body position-absolute bottom-0 p-4 p-md-6 rounded-bottom-3  text-neutral-0">
+                <div class="d-flex justify-content-between align-items-center mb-4 mb-md-6">
+                  <h5 class="card-title mb-0 fw-bold">
+                    山臻
+                  </h5>
+                  <div class="d-flex justify-content-between gap-4 text-neutral-40">
+                    <span class="fw-bold">SUN-MON</span>
+                    <span class="fw-bold">11:30 - 20:30</span>
+                  </div>
+                </div>
+                <p class="card-text fw-medium">
+                  帶您進入一次辣味與鮮香兼具的川菜美食之旅。我們的廚師掌握正宗的川菜烹調技巧，從麻辣鍋到口水雞，每一道菜都有其獨特的風味，讓您回味無窮。
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-10 col-md-6 col-xl-4">
+            <div
+              class="card position-relative border-0 rounded-3"
+            >
+              <picture>
+                <source
+                  srcset="@/assets/images/home-food-4.png"
+                  media="(min-width: 576px)"
+                >
+                <img
+                  class="w-100 rounded-3"
+                  src="@/assets/images/home-food-sm-4.png"
+                  alt="月永"
+                >
+              </picture>
+              <div class="card-body position-absolute bottom-0 p-4 p-md-6 rounded-bottom-3  text-neutral-0">
+                <div class="d-flex justify-content-between align-items-center mb-4 mb-md-6">
+                  <h5 class="card-title mb-0 fw-bold">
+                    月永
+                  </h5>
+                  <div class="d-flex justify-content-between gap-4 text-neutral-40">
+                    <span class="fw-bold">SUN-MON</span>
+                    <span class="fw-bold">11:00 - 20:00</span>
+                  </div>
+                </div>
+                <p class="card-text fw-medium">
+                  從鮮美的海鮮、經典的牛排，到各國的特色美食，我們都一應俱全。在這裡，您可以品嚐到世界各地的美食，每一道菜都由專業廚師用心製作，讓您在享受美食的同時，也能感受到我們的熱情與用心。
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-10 col-md-6 col-xl-4">
+            <div
+              class="card position-relative border-0 rounded-3"
+            >
+              <picture>
+                <source
+                  srcset="@/assets/images/home-food-5.png"
+                  media="(min-width: 576px)"
+                >
+                <img
+                  class="w-100 rounded-3"
+                  src="@/assets/images/home-food-sm-5.png"
+                  alt="天潮"
+                >
+              </picture>
+              <div class="card-body position-absolute bottom-0 p-4 p-md-6 rounded-bottom-3  text-neutral-0">
+                <div class="d-flex justify-content-between align-items-center mb-4 mb-md-6">
+                  <h5 class="card-title mb-0 fw-bold">
+                    天潮
+                  </h5>
+                  <div class="d-flex justify-content-between gap-4 text-neutral-40">
+                    <span class="fw-bold">SUN-MON</span>
+                    <span class="fw-bold">14:00 - 19:30</span>
+                  </div>
+                </div>
+                <p class="card-text fw-medium">
+                  我們提供各種精緻甜點與糕點，無論您喜歡的是巧克力蛋糕、法式馬卡龍，還是台灣傳統的糕點，都能在這裡找到。讓我們的甜點帶您進入一場繽紛的甜蜜旅程。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -537,6 +712,50 @@ section .btn {
 
   @include media-breakpoint-up(xxxl) {
     flex-grow: 1;
+  }
+}
+
+
+.delicacy::before {
+  @include media-breakpoint-up(lg) {
+    position: absolute;
+    top: -40px;
+    right: 80px;
+    content: '';
+    width: 200px;
+    height: 200px;
+    background-image: url('../assets/images/deco-dot-group.svg');
+  }
+}
+
+.delicacy::after {
+  @include media-breakpoint-up(xxxl) {
+    position: absolute;
+    top: 55px;
+    left: 40px;
+    content: '';
+    width: 187px;
+    height: 1068px;
+    background-image: url('@/assets/images/deco-line-group-vertical.svg');
+  }
+}
+
+.delicacy .deco-line {
+  width: 200px;
+
+  @include media-breakpoint-up(md) {
+    width: 167px;
+  }
+}
+
+.delicacy .card-body {
+  background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #140F0A 77.6%);
+  backdrop-filter: blur(10px);
+}
+
+.delicacy span {
+  @include media-breakpoint-down(md) {
+    font-size: 14px;
   }
 }
 
