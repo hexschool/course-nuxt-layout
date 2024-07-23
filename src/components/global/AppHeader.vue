11a6cee5ec671b667d35378d7fd38fbd4b0f7b64
Jay <kred110396@gmail.com>
2024-07-24 01:22:27 +0800

    refactor: 💡 move custom navbar styles to AppHeader

--- src/components/global/AppHeader.vue
+++ src/components/global/AppHeader.vue
@@ -105,6 +105,18 @@ onUnmounted(() => {
 </template>
 
 <style lang="scss" scoped>
+@import "bootstrap/scss/mixins/breakpoints";
+
+$grid-breakpoints: (
+  xs: 0,
+  sm: 576px,
+  md: 768px,
+  lg: 992px,
+  xl: 1200px,
+  xxl: 1400px,
+  xxxl: 1537px
+);
+
 .logo {
   max-width: 27vw;
 }
@@ -119,12 +131,65 @@ header {
 
 header.scrolled {
   background-color: #000;
-}
-
-@media (min-width: 768px) {
-  header.scrolled {
+  @include media-breakpoint-up(md) {
     .navbar {
       transform: scale(0.95);
+    }
+  }
+}
+
+@include media-breakpoint-down(md) {
+  .navbar-toggler {
+    z-index: 1;
+    visibility: hidden;
+
+    svg {
+      transition: opacity .3s;
+    }
+    
+    svg:nth-child(1) {
+      position: absolute;
+      top: 28px;
+      right: 28px;
+      opacity: 1;
+      visibility: visible;
+    }
+    svg:nth-child(2) {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+  .navbar-toggler.collapsed {
+    svg:nth-child(1) {
+      opacity: 0;
+      visibility: hidden;
+    }
+    svg:nth-child(2) {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+
+  .navbar-collapse {
+    min-height: 100vh;
+    background-color: #140f0a;
+    position: fixed;
+    inset: 0;
+    opacity: 0;
+    overflow: hidden;
+    transition: opacity .05s;
+  }
+  .navbar-collapse.show {
+    opacity: 1;
+  }
+  .navbar-nav {
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    a {
+      width: 90vw;
     }
   }
 }
