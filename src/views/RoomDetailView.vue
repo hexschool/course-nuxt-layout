a709902b7bb6e79c5e575c5115c3072a8007dee2
Jay <kred110396@gmail.com>
2024-07-24 20:57:48 +0800

    feat: 🎸 add booking form section to room detail page

--- src/views/RoomDetailView.vue
+++ src/views/RoomDetailView.vue
@@ -386,7 +386,120 @@ import { Icon } from '@iconify/vue';
               </ol>
             </section>
           </div>
+          <div class="d-none d-md-block col-md-5">
+            <div
+              class="rounded-3xl position-sticky d-flex flex-column gap-10 p-10 ms-auto bg-neutral-0"
+              style="top: 160px; max-width: 478px;"
+            >
+              <h5 class="pb-4 mb-0 text-neutral-100 fw-bold border-bottom border-neutral-40">
+                預訂房型
+              </h5>
+
+              <div class="text-neutral-80">
+                <h2 class="fw-bold">
+                  尊爵雙人房
+                </h2>
+                <p class="mb-0 fw-medium">
+                  享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。
+                </p>
+              </div>
+
+              <div>
+                <div class="d-flex flex-wrap gap-2 mb-4">
+                  <div class="form-floating flex-grow-1 flex-shrink-1">
+                    <input
+                      id="checkinInput"
+                      type="date"
+                      value="2023-12-03"
+                      class="form-control p-4 pt-9 text-neutral-100 fw-medium border-neutral-100 rounded-3"
+                      style="min-height: 74px;"
+                      placeholder="yyyy-mm-dd"
+                    >
+                    <label
+                      class="text-neutral-80 fw-medium"
+                      style="top: 8px;left: 8px;"
+                      for="checkinInput"
+                    >入住
+                    </label>
+                  </div>
+
+                  <div class="form-floating flex-grow-1 flex-shrink-1">
+                    <input
+                      id="checkoutInput"
+                      type="date"
+                      value="2023-12-04"
+                      class="form-control p-4 pt-9 text-neutral-100 fw-medium border-neutral-100 rounded-3"
+                      style="min-height: 74px;"
+                      placeholder="yyyy-mm-dd"
+                    >
+                    <label
+                      class="text-neutral-80 fw-medium"
+                      style="top: 8px;left: 8px;"
+                      for="checkoutInput"
+                    >退房
+                    </label>
+                  </div>
+                </div>
+
+                <div class="d-flex justify-content-between align-items-center text-neutral-100">
+                  <p class="mb-0">
+                    人數
+                  </p>
+                  <div class="d-flex align-items-center gap-4">
+                    <button
+                      class="btn btn-neutral-0 p-4 border border-neutral-40 rounded-circle"
+                      type="button"
+                    >
+                      <Icon
+                        class="fs-5 text-neutral-100"
+                        icon="ic:baseline-minus"
+                      />
+                    </button>
+
+                    <h6
+                      id="people"
+                      class="d-flex justify-content-center align-items-center fw-bold text-neutral-100"
+                      style="width: 24px;"
+                      name="people"
+                    >
+                      2
+                    </h6>
+
+                    <button
+                      class="btn btn-neutral-0 p-4 border border-neutral-40 rounded-circle"
+                      type="button"
+                    >
+                      <Icon
+                        class="fs-5 text-neutral-100"
+                        icon="ic:baseline-plus"
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <h5 class="mb-0 text-primary-100 fw-bold">
+                NT$ 10,000
+              </h5>
+              <button
+                class="btn btn-primary-100 py-4 text-neutral-0 fw-bold rounded-3"
+                type="button"
+              >
+                立即預訂
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
+      
+      <div class="d-flex d-md-none justify-content-between align-items-center position-fixed bottom-0 w-100 p-3 bg-neutral-0">
+        <small class="text-neutral-80 fw-medium">ＮＴ$ 10,000 / 晚</small>
+        <button
+          class="btn btn-primary-100 px-12 py-4 text-neutral-0 fw-bold rounded-3"
+          type="button"
+        >
+          查看可訂日期
+        </button>
       </div>
     </section>
   </main>
